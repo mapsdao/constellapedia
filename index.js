0876662df97511ae8a4123f5a3488003fc59c74f
Domingo <ing.domingos.l@gmail.com>
2022-06-05 22:32:46 +0200

    Clean up

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
-const tagLabel = 'APIEntryPoint';
-const GwApiV2 = require("gwapiv2-node-client");
+const tagLabel = 'APPEntryPoint';
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,67 +8,45 @@ const cors = require('cors');
 const glob = require('glob');
 const path = require("path");
 
-const api = express();
+
 const app = express();
-const site = express();
 
-global.gwApi = new GwApiV2(process.env.NODE_ENV, 'test', process.env.GW_BA_USER, process.env.GW_BA_PASSWORD);
 
-api.use(bodyParser.json({limit: '1mb'}));
-api.use(cors());
-api.options('*', cors());
 
 app.set('views', path.join(__dirname, 'public'));
 app.set('view engine', 'html');
 app.engine('html',require('ejs').renderFile);
 
 app.use(express.static('public'));
-site.use(express.static('site'));
 
-global.utilities = require('@growishpay/service-utilities');
 global.app = app;
-
-utilities.express.init(api);
-
-//Loads plugins, services, models and controllers
-const patterns = [
-    "api/services/*.js",
-    "api/controllers/**/*.js"
-];
-
-for(const pattern of patterns) {
-    const files = glob.sync(pattern,null);
-
-    for (const filePath of files) {
-        require("./" + filePath);
-    }
-}
-
-api.use('*', (req, res) => res.notFound());
+global.utilities = require('@growishpay/service-utilities');
 
 
-api.listen(process.env.API_PORT, async () => {
-    await gwApi.Domain.read();
-    utilities.logger.info("Connection with GrowishPay API v2 OK", {tagLabel});
+app.get('/visualizer/:dataNFT', (req, res) => {
 
-    utilities.logger.info("API Server ready!", {tagLabel, port: process.env.API_PORT});
+    res.render('visualizer', {
+        dataNFT: req.params.dataNFT,
+        title: "Foo",
+        options: Buffer.from(JSON.stringify({
+            edges: {
+                font: {
+                    size: 15
+                }
+            }
+        })).toString('base64'),
+        source: {
+            name: "Test",
+            url: "https://google.com"
+        }
+    });
+
 });
 
-app.get('/', (req, res)=>{
-    res.render('index', {});
+app.get('/json-test/foo.json', (req, res)=>{
+    res.json(require('./json-example-data.json'));
 });
-
-require('./app/controllers/constellation');
-require('./app/controllers/node');
 
 app.listen(process.env.APP_PORT, () => {
     utilities.logger.info("APP Server ready!", {tagLabel, port: process.env.APP_PORT});
 });
-
-site.listen(process.env.SITE_PORT, () => {
-    utilities.logger.info("SITE Server ready!", {tagLabel, port: process.env.SITE_PORT});
-});
-
-//constellapedia.com
-//app.constellapedia.com
-//api.constellapedia.com
