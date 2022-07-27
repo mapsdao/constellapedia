942f60a4b118dcd601489bc10cc6275d1e5d5268
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-07-27 08:45:23 +0200

    show dataNFTtag as title

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ global.app = app;
 global.utilities = require('@growishpay/service-utilities');
 
 
-app.get('/visualizer/:dataNFT', (req, res) => {
+app.get('/visualizer/:dataNFTtag', (req, res) => {
 
     res.render('visualizer', {
-        dataNFT: req.params.dataNFT,
-        title: "Foo",
+        dataNFTtag: req.params.dataNFTtag,
+        title: req.params.dataNFTtag,
         options: Buffer.from(JSON.stringify({
             autoResize: true,
             nodes: {
